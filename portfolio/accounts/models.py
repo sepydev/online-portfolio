69448b4048de49a984097838621f195f81449a87
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-04-26 21:43:07 +0430

    Create a user profile when a new user is created

--- portfolio/accounts/models.py
+++ portfolio/accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class User(AbstractUser):
@@ -9,7 +11,15 @@ class User(AbstractUser):
         verbose_name = 'User'
 
 
+@receiver(post_save, sender=User)
+def insert_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
 class Profile(models.Model):
     user = models.OneToOneField('User', models.CASCADE)
     job_title = models.CharField(max_length=155, verbose_name="Job title", blank=True, default='')
 
+    def __str__(self):
+        return self.user.username
